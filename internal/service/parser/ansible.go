30024df381e83ac66c36724940dcc82216230a22
donbasta <faddilafarras@gmail.com>
2021-11-29 10:05:21 +0700

    feat: ansible yaml parser

--- internal/service/parser/ansible.go
+++ internal/service/parser/ansible.go
@@ -15,28 +15,28 @@ func MakeVersion(semver string) Semver {
 	return Semver{major, minor, patch}
 }
 
-func (d *dependencyParser) Parse(fileContent string) ([]Dependency, error) {
-	lines := strings.Split(fileContent, "\n")
-	deps := []Dependency{}
-	buffer := Dependency{}
-	for i := 1; i < len(lines); i++ {
-		if len(lines[i]) == 0 {
-			continue
-		}
-		tmpLine := lines[i]
-		if lines[i][0] == '-' {
-			buffer = Dependency{}
-			tmpLine = tmpLine[1:]
-		}
-		tmpLine = strings.Trim(tmpLine, " ")
-		tokens := strings.Split(tmpLine, ":")
-		attr := tokens[0]
-		if attr == "src" {
-			buffer.Url = tokens[1]
-		} else if attr == "version" {
-			buffer.Version = MakeVersion(tokens[1])
-			deps = append(deps, buffer)
-		}
-	}
-	return deps, nil
-}
+// func (d *dependencyParser) Parse(fileContent string) ([]Dependency, error) {
+// 	lines := strings.Split(fileContent, "\n")
+// 	deps := []Dependency{}
+// 	buffer := Dependency{}
+// 	for i := 1; i < len(lines); i++ {
+// 		if len(lines[i]) == 0 {
+// 			continue
+// 		}
+// 		tmpLine := lines[i]
+// 		if lines[i][0] == '-' {
+// 			buffer = Dependency{}
+// 			tmpLine = tmpLine[1:]
+// 		}
+// 		tmpLine = strings.Trim(tmpLine, " ")
+// 		tokens := strings.Split(tmpLine, ":")
+// 		attr := tokens[0]
+// 		if attr == "src" {
+// 			buffer.Url = tokens[1]
+// 		} else if attr == "version" {
+// 			buffer.Version = MakeVersion(tokens[1])
+// 			deps = append(deps, buffer)
+// 		}
+// 	}
+// 	return deps, nil
+// }
